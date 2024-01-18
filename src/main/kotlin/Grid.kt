8629bc44def58019806a1108197434965cceca76
Grant Kurtz <th3guys2@gmail.com>
2024-01-18 17:15:36 -0500

    Refactored Grid to use a ContextObject.

--- src/main/kotlin/Grid.kt
+++ src/main/kotlin/Grid.kt
@@ -2,31 +2,42 @@ import com.varabyte.kotter.foundation.render.OffscreenCommandRenderer
 import com.varabyte.kotter.foundation.render.offscreen
 import com.varabyte.kotter.foundation.text.text
 import com.varabyte.kotter.foundation.text.textLine
+import com.varabyte.kotter.runtime.Section
+import com.varabyte.kotter.runtime.concurrent.createKey
 import com.varabyte.kotter.runtime.render.OffscreenRenderScope
 import com.varabyte.kotter.runtime.render.RenderScope
 
+data class GridContext(
+    val width: Int,
+    val columns: Int,
+    var cellIndex: Int,
+    var previousBuffers: MutableList<Pair<List<Int>, OffscreenCommandRenderer>> = mutableListOf()
+)
+
+val gridContextKey = Section.Lifecycle.createKey<GridContext>()
+
 fun RenderScope.cell(render: OffscreenRenderScope.() -> Unit) {
-    val cellPos = data[cellIndex] ?: 0
-    val width = data[colWidth] ?: 0
-    val columns = data[columnCount] ?: 0
-    val previousBuffers: MutableList<Pair<List<Int>, OffscreenCommandRenderer>> = data[prevBuffers] ?: mutableListOf()
+    val cellPos = data[gridContextKey]!!.cellIndex
+    val width = data[gridContextKey]!!.width
+    val columns = data[gridContextKey]!!.columns
+    val previousBuffers = data[gridContextKey]!!.previousBuffers
 
     val content = offscreen(render)
     val renderer = content.createRenderer()
     previousBuffers.add(Pair(content.lineLengths, renderer))
 
-    data[prevBuffers] = previousBuffers
-    if (data[cellIndex]!! == columns - 1) {
+    data[gridContextKey]!!.previousBuffers = previousBuffers
+    if (data[gridContextKey]!!.cellIndex == columns - 1) {
         flushCells(width)
-        data[cellIndex] = 0
+        data[gridContextKey]!!.cellIndex = 0
     }
     else {
-        data[cellIndex] = cellPos + 1
+        data[gridContextKey]!!.cellIndex = cellPos + 1
     }
 }
 
 fun RenderScope.flushCells(width: Int) {
-    val previousBuffers: MutableList<Pair<List<Int>, OffscreenCommandRenderer>> = data[prevBuffers] ?: mutableListOf()
+    val previousBuffers = data[gridContextKey]!!.previousBuffers
     while (hasNextRows(previousBuffers)) {
         previousBuffers.forEachIndexed { index, buf ->
             val renderer = buf.second
@@ -42,17 +53,14 @@ fun RenderScope.flushCells(width: Int) {
         textLine()
     }
 
-    data[prevBuffers] = mutableListOf()
+    data[gridContextKey]!!.previousBuffers = mutableListOf()
 }
 
 fun hasNextRows(previousBuffers: MutableList<Pair<List<Int>, OffscreenCommandRenderer>>):
         Boolean = previousBuffers.fold(false) { anyHas, buf -> anyHas || buf.second.hasNextRow() }
 
 fun RenderScope.grid(width: Int, columns: Int, render: OffscreenRenderScope.() -> Unit) {
-    data[colWidth] = width
-    data[columnCount] = columns
-    data[cellIndex] = 0
-    data[prevBuffers] = mutableListOf()
+    data[gridContextKey] = GridContext(width, columns, 0)
 
     val content = offscreen(render)
     val renderer = content.createRenderer()
